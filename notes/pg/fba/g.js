433e7bb665d2dc4a7f2980a5d0c7e582d46842e6
KAMAL SANKHLA <44642421+ultra-precise@users.noreply.github.com>
2018-12-13 18:02:18 +0530

    Delete g.js

--- notes/pg/fba/g.js
+++
@@ -1,6 +0,0 @@
-function receiveMessage(event) {
-  if (event.origin !== "https://www.bing.com")
-    return;
-console.log(event.data);
-}
-window.addEventListener("message", receiveMessage, false);
