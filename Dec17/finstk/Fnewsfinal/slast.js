93e1deaf458696b5cb02f5218c5ce9f77fdab0ac
KAMAL SANKHLA <44642421+ultra-precise@users.noreply.github.com>
2019-01-04 05:35:41 +0530

    Create slast.js

---
+++ Dec17/finstk/Fnewsfinal/slast.js
@@ -0,0 +1,7 @@
+//var pew = window.open("https://finviz.com/?q=plugin+enabled", "FinNews", "width=1066,height=628,resizable=yes,scrollbars=yes,toolbar=yes,menubar=yes,location=yes,directories=no, status=yes");
+
+
+window.onload = function(){
+var gnwez = document.querySelector("a#gnews");
+gnwez.click();
+};
